9fe712e3c72d8910605322aac86f0c267f45a3bd
Andrew Ruef <2530731+awruef@users.noreply.github.com>
2020-03-07 14:09:27 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("expected statefile logfile");
+    }
+
+    let statefile = &args[1];
+    let logfile = &args[2];
+    println!("statefile == {} logfile == {}", statefile, logfile);
+
+    let logfile_contents = fs::read_to_string(logfile)
+        .expect("Error opening log file");
+    let lines: Vec<&str> = logfile_contents.split("\n").collect();
+
+    for line in &lines {
+        println!("{}", line);
+    }
 }
